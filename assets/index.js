aff682778d2a26903e39590c33225c86b51be32a
matthew <matthkang@gmail.com>
2023-08-12 21:00:54 -0700

    install inquirer, list of questions

--- assets/index.js
+++ assets/index.js
@@ -1,13 +1,28 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer')
+const fs = require('fs')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    { message: 'What is the project title?', type: 'input', name: 'title' },
+    { message: 'What is the project description?', type: 'input', name: 'desc' },
+    { message: 'What are the installation instructions?', type: 'input', name: 'install' },
+    { message: 'What is the usage info? (instructions/examples for use)', type: 'input', name: 'usage' },
+    { message: 'What are the contribution guidelines?', type: 'input', name: 'contribute' },
+    { message: 'What are the test instructions?', type: 'input', name: 'test' },
+];
+
+inquirer.prompt(questions)
+.then((answers) => {
+    console.log(answers);
+})
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
